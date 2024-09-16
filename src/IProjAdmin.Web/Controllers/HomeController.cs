65863bf22e6ece75e2580e41a6ff8ce3b71073f2
SamandarbekYr <SamandarbekYR@gmail.com>
2024-09-16 17:13:50 +0500

    remove logger

--- src/IProjAdmin.Web/Controllers/HomeController.cs
+++ src/IProjAdmin.Web/Controllers/HomeController.cs
@@ -8,56 +8,45 @@ namespace IProjAdmin.Web.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private ILogger _logger;
 
-        public HomeController(ILogger logger)
-        {
-            _logger = logger;
-        }
         public async Task<IActionResult> Index()
         {
-            try
-            {
-                var accessToken = await HttpContext.GetTokenAsync("access_token");
-                var claimsPrincipal = accessToken!.DecodeJwtToken();
-                var userRole = claimsPrincipal.GetRole();
-                (string? Gmail, Guid? userId) = claimsPrincipal.GetEmailAndId();
 
-                if (userRole != null && !string.IsNullOrEmpty(Gmail))
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var claimsPrincipal = accessToken!.DecodeJwtToken();
+            var userRole = claimsPrincipal.GetRole();
+            (string? Gmail, Guid? userId) = claimsPrincipal.GetEmailAndId();
+
+            if (userRole != null && !string.IsNullOrEmpty(Gmail))
+            {
+
+                HttpContext.Response.Cookies.Append("UserGmail", Gmail, new CookieOptions
                 {
+                    HttpOnly = true,
+                    Expires = DateTimeOffset.UtcNow.AddDays(7)
+                });
 
-                    HttpContext.Response.Cookies.Append("UserGmail", Gmail, new CookieOptions
+                if (userRole.Equals("Owner"))
+                {
+                    if (!string.IsNullOrEmpty(Gmail) && userId != null)
                     {
-                        HttpOnly = true,
-                        Expires = DateTimeOffset.UtcNow.AddDays(7)
-                    });
-
-                    if (userRole.Equals("Owner"))
-                    {
-                        if (!string.IsNullOrEmpty(Gmail) && userId != null)
+                        HttpContext.Response.Cookies.Append("BossId", userId.ToString()!, new CookieOptions
                         {
-                            HttpContext.Response.Cookies.Append("BossId", userId.ToString()!, new CookieOptions
-                            {
-                                HttpOnly = true,
-                                Expires = DateTimeOffset.UtcNow.AddDays(7)
-                            });
-                        }
-
-                        return RedirectToAction("Main", "Admin");
+                            HttpOnly = true,
+                            Expires = DateTimeOffset.UtcNow.AddDays(7)
+                        });
                     }
 
-                    else
-                    {
-                        return Redirect("https://iproj.uz");
-                    }
+                    return RedirectToAction("Main", "Admin");
                 }
-                return View();
+
+                else
+                {
+                    return Redirect("https://iproj.uz");
+                }
             }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Home controllerdagi indexda xatolik yuz berdi: {ex}");
-                return View();
-            }
+            return View();
         }
+
     }
 }
